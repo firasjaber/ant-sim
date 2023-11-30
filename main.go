78ac6cea796f376fe8aa0238796e1f869543d64c
Firas Jaber <firasjaberc@gmail.com>
2023-11-30 18:21:06 +0100

    basic scene setup

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/firasjaber/ant-sim/entity"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func updateAnts(ants []*entity.Ant) {
+	for _, ant := range ants {
+		ant.Update()
+	}
+}
+
+func init() {
+	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	rl.SetTargetFPS(60)
+	rl.SetExitKey(0) 
+}
+
+func main() {
+	// spawn an ant
+	ant := entity.NewAnt(50, 50)
+
+	// build ants list
+	ants := []*entity.Ant{ant}
+
+	for !rl.WindowShouldClose() {
+		// begin the drawing and clear the screen
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+		
+		// update the entities
+		updateAnts(ants)
+
+		// end the drawing
+		rl.EndDrawing()
+	}
+
+	rl.CloseWindow()
+}
