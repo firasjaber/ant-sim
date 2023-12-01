e7bd5dd568d3590fc7b37bc0722e8ca016eb7f48
Firas Jaber <firasjaberc@gmail.com>
2023-12-01 21:05:22 +0100

    add a simple HUD

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/firasjaber/ant-sim/config"
 	"github.com/firasjaber/ant-sim/entity"
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -18,10 +20,20 @@ func updateFood(food []*entity.Food) {
 	}
 }
 
+func updateHud(home *entity.Home, fps int32) {
+	spawnedFood := "Spawned food: " + strconv.FormatInt(int64(config.FoodCount), 10)
+	collectedFood := "Collected food: " + strconv.FormatInt(int64(home.GetFoodCount()), 10)
+	fpsText := "FPS: " + strconv.FormatInt(int64(fps), 10)
+
+	rl.DrawText(spawnedFood, 10, 10, 15, rl.LightGray)
+	rl.DrawText(collectedFood, 10, 30, 15, rl.LightGray)
+	rl.DrawText(fpsText, 10, 50, 15, rl.LightGray)
+}
+
 func init() {
 	rl.InitWindow(config.WindowWidth, config.WindowHeight, config.WindowTitle)
 	rl.SetTargetFPS(config.TargetFPS)
-	rl.SetExitKey(0) 
+	rl.SetExitKey(0)
 }
 
 func main() {
@@ -44,8 +56,8 @@ func main() {
 	// loop through the food number range and create new food on random positions
 	food := []*entity.Food{}
 	for i := 0; i < config.FoodCount; i++ {
-	 		// create a new food
-		f := entity.NewFood(int32(rl.GetRandomValue(10, 190)), int32(rl.GetRandomValue(10, 190)))
+		// create a new food
+		f := entity.NewFood(int32(rl.GetRandomValue(10, config.WindowWidth-10)), int32(rl.GetRandomValue(10, config.WindowHeight-10)))
 		// add the food to the food list
 		food = append(food, f)
 	}
@@ -54,7 +66,10 @@ func main() {
 		// begin the drawing and clear the screen
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
-		
+
+		// draw hud
+		updateHud(home, rl.GetFPS())
+
 		// update the entities
 		updateFood(food)
 		updateAnts(ants, food, home)
