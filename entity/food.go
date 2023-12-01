15c3822bc87b7a29aece29059182b782f4ebbd0a
Firas Jaber <firasjaberc@gmail.com>
2023-12-01 21:05:45 +0100

    add destroyed property to food

--- entity/food.go
+++ entity/food.go
@@ -5,10 +5,11 @@ import rl "github.com/gen2brain/raylib-go/raylib"
 type Food struct {
 	posX int32
 	posY int32
+	destroyed bool
 }
 
 func NewFood(posX int32, posY int32) *Food {
-	return &Food{posX: posX, posY: posY}
+	return &Food{posX: posX, posY: posY, destroyed: false}
 }
 
 func (f *Food) Update() {
@@ -16,9 +17,24 @@ func (f *Food) Update() {
 }
 
 func (f *Food) Draw() {
+	if (f.destroyed) {
+		return
+	}
 	rl.DrawRectangle(f.posX, f.posY, 5, 5, rl.Green)
 }
 
 func (f *Food) GetPosition() rl.Vector2 {
 	return rl.Vector2{X: float32(f.posX), Y: float32(f.posY)}
+}
+
+func (f *Food) Destroy() {
+	f.destroyed = true
+}
+
+func (f *Food) IsDestroyed() bool {
+	return f.destroyed
+}
+
+func (a *Food) GetRectangle() rl.Rectangle {
+	return rl.Rectangle{X: float32(a.posX), Y: float32(a.posY), Width: 5, Height: 5}
 }
