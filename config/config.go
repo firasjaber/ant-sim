8a88b5b873570a9b92f9651c40f3635e9e3fded4
Firas Jaber <firasjaberc@gmail.com>
2023-12-12 15:53:40 +0100

    working config

--- config/config.go
+++ config/config.go
@@ -2,14 +2,14 @@ package config
 
 const (
 	// Window
-	WindowWidth  = 500
-	WindowHeight = 500
+	WindowWidth  = 700
+	WindowHeight = 700
 	WindowTitle  = "Ant Colony Simulation"
-	TargetFPS    = 60
+	TargetFPS    = 90
 	// Ants
 	AntsCount          = 30
 	WanderingRate      = 0.05
-	PheromoneDecayRate = 0.15
+	PheromoneDecayRate = 0.1
 	// Food
 	FoodCount int = 20
 )
