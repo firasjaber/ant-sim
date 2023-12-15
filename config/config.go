94f99412dee0f419f3a86f8adf1cea3feb512f52
Firas Jaber <firasjaberc@gmail.com>
2023-12-15 07:15:09 +0100

    dummy configs

--- config/config.go
+++ config/config.go
@@ -1,15 +1,47 @@
 package config
 
+// const (
+// 	// Window
+// 	WindowWidth  = 300
+// 	WindowHeight = 300
+// 	WindowTitle  = "Ant Colony Simulation"
+// 	TargetFPS    = 120
+// 	// Ants
+// 	AntsCount          = 10
+// 	WanderingRate      = 0.05
+// 	PheromoneDecayRate = 0.2
+// 	// Food
+// 	FoodCount int = 20
+// DrawPheromones = false
+// )
+
+// const (
+// 	// Window
+// 	WindowWidth  = 300
+// 	WindowHeight = 300
+// 	WindowTitle  = "Ant Colony Simulation"
+// 	TargetFPS    = 45
+// 	// Ants
+// 	AntsCount          = 10
+// 	WanderingRate      = 0.05
+// 	PheromoneDecayRate = 0.2
+// 	// Food
+// 	FoodCount      int = 20
+// 	DrawPheromones     = true
+// )
+
 const (
 	// Window
-	WindowWidth  = 700
-	WindowHeight = 700
+	WindowWidth  = 400
+	WindowHeight = 400
 	WindowTitle  = "Ant Colony Simulation"
-	TargetFPS    = 90
+	TargetFPS    = 60
 	// Ants
-	AntsCount          = 30
+	AntsCount          = 40
 	WanderingRate      = 0.05
-	PheromoneDecayRate = 0.1
+	PheromoneDecayRate = 0.2
 	// Food
-	FoodCount int = 20
+	FoodCount      int = 20
+	DrawPheromones     = true
+	DrawHUD            = true
 )
